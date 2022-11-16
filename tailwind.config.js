8ccf38671e894e3a06869638d06fc7d67628d732
John U <arjorb@gmail.com>
2022-11-16 17:33:57 +0200

    done working on the navbar and showcase

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ['./*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'gradient-xy': 'gradient-xy 15s ease infinite',
+      },
+      keyframes: {
+        'gradient-xy': {
+          '0%, 100%': {
+            'background-size': '400% 400%',
+            'background-position': 'left center',
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center',
+          },
+        },
+      },
+      maxWidth: {
+        '5.5xl': '85vw',
+      },
+      height: {
+        full: '98.5vh',
+      },
+    },
   },
   plugins: [],
 };
